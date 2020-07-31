ada13d9c18e1859778ee865318f68f6e486ab03c
king-11 <lakshay.singh1108@gmail.com>
2020-07-31 23:24:32 +0530

    paths protected

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
 import { connect } from './utils/db'
+import { signup, signin, protect } from './utils/auth'
+
 import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
 import listRouter from './resources/list/list.router'
@@ -16,7 +18,10 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.post('/signup', signup)
+app.post('/signin', signin)
 
+app.use('/api', protect)
 app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/list', listRouter)
