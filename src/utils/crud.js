e5a02d9bfaef7e373cdbefb13c2d8dcc6b056dd4
king-11 <lakshay.singh1108@gmail.com>
2020-07-31 22:17:05 +0530

    exercise 4 crud generic controllers setup

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,65 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const data = await model.findOne({ _id: id, createdBy: userId }).exec()
 
-export const getMany = model => async (req, res) => {}
+  if (!data) {
+    return res.status(404).end()
+  }
 
-export const createOne = model => async (req, res) => {}
+  res.status(200).json({ data })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user._id
+  const data = await model.find({ createdBy: userId }).exec()
+  if (data) {
+    return res.status(200).json({ data })
+  }
 
-export const removeOne = model => async (req, res) => {}
+  res.status(404).end()
+}
+
+export const createOne = model => async (req, res) => {
+  const userId = req.user._id
+  const body = req.body
+
+  try {
+    const data = await model.create({ createdBy: userId, ...body })
+    return res.status(201).json({ data })
+  } catch (err) {
+    return res.status(400).end()
+  }
+}
+
+export const updateOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const update = req.body
+
+  try {
+    const data = await model.findOneAndUpdate(
+      { createdBy: userId, _id: id },
+      update,
+      { new: true }
+    )
+    return res.status(200).json({ data })
+  } catch (err) {
+    return res.status(400)
+  }
+}
+
+export const removeOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  try {
+    const data = await model.findOneAndRemove({ createdBy: userId, _id: id })
+    return res.status(200).json({ data })
+  } catch (err) {
+    return res.status(400)
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
