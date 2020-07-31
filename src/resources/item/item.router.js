005beabdf1ea3e0524766e57688d4218e142e99a
king-11 <lakshay.singh1108@gmail.com>
2020-07-31 15:25:02 +0530

    exercise 2 make routes using router done

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,20 @@ import { Router } from 'express'
 
 const router = Router()
 
+const controller = (req, res) => {
+  res.set(200)
+  res.send({ hey: 'there' })
+}
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
+
 export default router
